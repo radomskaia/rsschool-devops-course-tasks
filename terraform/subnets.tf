2414a90ab0ae1013be1f67f702b0fc1bf4ba626a
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-06-18 14:23:32 +0700

    feat(terraform):  setup public/private subnets with routing and NAT

---
+++ terraform/subnets.tf
@@ -0,0 +1,26 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.vpc_name}-public-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "${var.vpc_name}-private-${count.index + 1}"
+  }
+}
